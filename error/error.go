b4d87f7b399c922341d39e380f8dd22190522d7f
chulinx <CiP4CJb9Yk8N9gE>
2020-11-07 09:32:10 +0800

    add error moudle

---
+++ error/error.go
@@ -0,0 +1,42 @@
+package error
+
+import (
+	"log"
+	"os"
+)
+
+
+type Error struct {
+	Prefix string
+}
+
+func NewError() *Error {
+	return &Error{}
+}
+
+func (e Error)SetPrefix(prefix string) *Error {
+	if prefix == ""{
+		prefix = "zlxGo"
+	}
+	e.Prefix = prefix
+	return &e
+}
+
+func (e Error)FatalError(err error,msg string)  {
+	if err != nil {
+		log.Fatalf("%s: %s--%s",e.Prefix,msg,err)
+	}
+}
+
+func (e Error)PrintfError(err error,msg string)  {
+	if err != nil {
+		log.Printf("%s: %s--%s",e.Prefix,msg,err)
+	}
+}
+
+func (e Error)FatalErrorExit(err error,msg string)  {
+	if err != nil {
+		log.Fatalf("%s: %s--%s",e.Prefix,msg,err)
+		os.Exit(1)
+	}
+}
