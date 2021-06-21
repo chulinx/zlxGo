621dc1252736649376ca7b00466e0c434a00012f
chulinx <chulinx@163.com>
2021-06-21 16:47:09 +0800

    add array set

--- array/array.go
+++ array/array.go
@@ -1,5 +1,7 @@
 package array
 
+// Array 去除 空元素
+
 func RemoveNil(arr []string) (newArr []string) {
 	for i :=range arr {
 		if len(arr[i]) >1 {
@@ -7,4 +9,18 @@ func RemoveNil(arr []string) (newArr []string) {
 		}
 	}
 	return newArr
+}
+
+// array 去重
+
+func Set(languages []string) []string {
+	result := make([]string, 0, len(languages))
+	temp := map[string]struct{}{}
+	for _, item := range languages {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
 }
