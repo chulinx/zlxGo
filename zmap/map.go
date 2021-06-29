11b1424852b1fd6d2e4af79847aad46c40db9d3f
chulinx <chulinx@163.com>
2021-06-29 17:13:53 +0800

    mergmap map[string]interface{}

--- zmap/map.go
+++ zmap/map.go
@@ -1,7 +1,7 @@
 package zmap
 
-func MergeMap(mObj ...map[string]string) map[string]string {
-	newObj := map[string]string{}
+func MergeMap(mObj ...map[string]interface{}) map[string]interface{} {
+	newObj := map[string]interface{}{}
 	for _, m := range mObj {
 		for k, v := range m {
 			newObj[k] = v
